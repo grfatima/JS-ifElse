2e9d83e0f1395ba4a36d7e15d50365d114b5ebf9
grfatime <kurbanovhafatime@gmail.com>
2025-02-21 00:29:40 +0400

    direction almaq

---
+++ Istiqamet almaq/app.js
@@ -0,0 +1,18 @@
+let direction = prompt("Itiqamet daxil edin: (up,right,down,left)").toLowerCase();
+
+if (direction == "up"){
+    console.log("Yuxari get")
+
+} else if (direction == "right"){
+    console.log("Saga-a get")
+
+} else if (direction == "down"){
+    console.log("Asagi get")
+
+}else if (direction == "left"){
+    console.log("Sol-a get")
+
+}else {
+    console.log("Istiqameti yanlis daxil etdiniz.")
+}
+
