4d882a468f34601307ba5b1a81f5f28cc8bf3764
grfatime <kurbanovhafatime@gmail.com>
2025-02-21 00:40:22 +0400

    name,gender,age

--- name,gender,age/app.js
+++ name,gender,age/app.js
@@ -1,16 +1,19 @@
 let name = prompt("Adinizi daxil edin:")
-let gender = prompt("Cinsinizi daxil edin (q/k):")
-let age = +(prompt("Yasinizi daxil edin:"))
+let gender = prompt("Cinsinizi daxil edin: (q/k)")
+let age = prompt("Yasinizi daxil edin:")
 
 if (age >= 18){
-    if (gender == k){
-        console.log("Xos gelmisiniz Cenab" + name)
+    if (gender === "k" || gender === "K"){
+        console.log("Xos gelmisiniz Cenab " + name)
 
-    } else if (gender == q){
-        console.log("Xos gelmisiniz Xanim" + name)
+    }else if (gender === "q" || gender === "Q"){
+        console.log("Xos gelmisiniz Xanim " + name)
+
+    }else {
+        console.log("Xos gelmisiniz " + name )
 
     }
-
 } else {
     console.log("Yasiniz 18-den kicikdir.")
+
 }
