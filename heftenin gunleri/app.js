f3abc351822b8d91b92351da7ab2d17460e569ba
grfatime <kurbanovhafatime@gmail.com>
2025-02-20 23:11:17 +0400

    heftenin gunleri siyahisi yaradildi.

---
+++ heftenin gunleri/app.js
@@ -0,0 +1,26 @@
+let gun = prompt("Heftenin necenci gunudur? (1-7 arasi reqem daxil edin)");
+
+if (gun == 1){
+    console.log("Bu gun Bazar ertesidir.");
+
+}else if (gun == 2){
+    console.log("Bu gun Cersenbe axsamidir.");
+
+}else if (gun == 3){
+    console.log("Bu gun Cersenbe gunudur.");
+
+}else if (gun == 4){
+    console.log("Bu gun Cume axsamidir.");
+
+}else if (gun == 5){
+    console.log("Bu gun Cume gunudur.");
+
+}else if (gun == 6){
+    console.log("Bu gun Senbe gunudur.");
+
+}else if (gun == 7){
+    console.log("Bu gun Bazar gunudur.");
+    
+}else {
+    console.log("1-7 arasi reqem daxil edin!");
+}
