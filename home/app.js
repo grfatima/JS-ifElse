244d29b3b73ded75b5264f440b9a9e862dd8cc4d
grfatime <kurbanovhafatime@gmail.com>
2025-02-21 00:16:29 +0400

    area,price

---
+++ home/app.js
@@ -0,0 +1,9 @@
+let evinKvadrati = prompt("Evin kvadratini daxil edin:");
+let qiymet = prompt("Evin qiymetini daxil edin:");
+
+if (evinKvadrati >= 2000 && qiymet == 99999){
+    console.log("Evi aliram.");
+
+}else {
+    console.log("Evi almiram.");
+}
